a7ff6e8b65f53d78959e5c3fa7ed60faf321be76
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2025-02-03 19:36:34 +0900

    Finish migrating away from mdsvex.

--- src/routes/(centered)/+page.svelte
+++ src/routes/(centered)/+page.svelte
@@ -1,15 +1,23 @@
+<script>
+	import { undent } from '$lib/undent'
+	import markdownit from 'markdown-it'
+
+	const html = markdownit({
+		linkify: true,
+	}).render(undent`
+        # John's Site
+        - [Resume](/resume)
+
+        ## vCard
+
+        <a href="/api/vcard" download><button>Download vCard as VCF file</button></a>
+
+        ### QR Code:
+
+        ![](/api/vcard?format=png)
+    `)
+</script>
+
 <article>
-
-# John's Site
-- [Resume](/resume)
-
-## vCard
-
-<a href="/api/vcard" download><button>Download vCard as VCF file</button></a>
-
-### QR Code:
-
-![](/api/vcard?format=png)
-
-
+	{@html html}
 </article>
