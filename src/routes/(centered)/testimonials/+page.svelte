bc327af1b0d553664f5b82d8da0d676a1de54ab4
Leftium <381217+Leftium@users.noreply.github.com>
2025-09-04 02:38:46 -0500

    Get markdown to prettify and render as desired.

--- src/routes/(centered)/testimonials/+page.svelte
+++ src/routes/(centered)/testimonials/+page.svelte
@@ -6,7 +6,7 @@
 	import centerText from 'markdown-it-center-text'
 	import deflist from 'markdown-it-deflist'
 
-	const md = makeTagFunctionMd({ html: true, linkify: true, typographer: true, breaks: true }, [
+	const md = makeTagFunctionMd({ html: true, linkify: true, typographer: true, breaks: false }, [
 		[attr],
 		[deflist],
 		[centerText],
@@ -20,37 +20,36 @@
 		From [William Albright](https://github.com/ewilliam), a fellow software developer
 		_(Minor edits for readability)_
 
-		> **I was burned out. React and JSX were kicking my ass, and I\
-		questioned whether web development was for me.** My mental\
-		model kept me stuck in the backend; I couldn't replicate the\
-		frontends I admired on the web.
+		> **I was burned out. React and JSX were kicking my ass, and I
+		> questioned whether web development was for me.** My mental
+		> model kept me stuck in the backend; I couldn't replicate the
+		> frontends I admired on the web.
 		>
-		> [Before working with John,] I had a sprawling schema with\
-		separate tables and columns for every individual rubric item—a\
-		big, slow mess. **Then John [simplified everything by]\
-		storing the entire rubric in JSON.** It never occurred to me\
-		that complex UI could be unlocked by the right data\
-		structures.
+		> [Before working with John,] I had a sprawling schema with
+		> separate tables and columns for every individual rubric item—a
+		> big, slow mess. **Then John [simplified everything by]
+		> storing the entire rubric in JSON.** It never occurred to me
+		> that complex UI could be unlocked by the right data
+		> structures.
 		>
-		> John's implementation of the “functional core, imperative\
-		shell” pattern is another thing that stuck with me—code\
-		that’s testable and modular, separate from the UI. **John read\
-		an article and then put it into practice. That inspired me\
-		to read more technical material.**
+		> John's implementation of the “functional core, imperative
+		> shell” pattern is another thing that stuck with me—code
+		> that’s testable and modular, separate from the UI. **John read
+		> an article and then put it into practice. That inspired me
+		> to read more technical material.**
 		>
-		> And John's logging strategies? **Quality of life improvements\
-		that punch above weight.**
+		> And John's logging strategies? **Quality of life improvements
+		> that punch above weight.**
 		>
-		> All of that helped shift my perspective on frontend\
-		development. I used to think frontend was less serious than\
-		backend. Now I think frontend is more fun, engaging, and\
-		beautiful.
+		> All of that helped shift my perspective on frontend
+		> development. I used to think frontend was less serious than
+		> backend. Now I think frontend is more fun, engaging, and
+		> beautiful.
 		>
-		> And finally—John's lack of ego, his willingness to\
-		collaborate with my team regardless of skill level—it\
-		humbled me. Killed off whatever diva energy I had left. **I’ve\
-		become a better builder because of John!**
-
+		> And finally—John's lack of ego, his willingness to
+		> collaborate with my team regardless of skill level—it
+		> humbled me. Killed off whatever diva energy I had left. **I’ve
+		> become a better builder because of John!**
 	`}
 </scope-css>
 
